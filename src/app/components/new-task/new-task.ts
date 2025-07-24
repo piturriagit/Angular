854bf47711fdbf7e61ad57a0b61b5e7162ba8bc4
piturria <patricia.iturriaganez@gmail.com>
2025-07-24 23:15:32 +0200

    adding a simple form

--- src/app/components/new-task/new-task.ts
+++ src/app/components/new-task/new-task.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TaskForm } from "../task-form/task-form";
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [TaskForm],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css'
 })
