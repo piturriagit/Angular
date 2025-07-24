475ec63919f2e3379cd4cecd2bb371422958017d
piturria <patricia.iturriaganez@gmail.com>
2025-07-24 22:29:27 +0200

    adding info to footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,18 @@
+import { DatePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './footer.html',
   styleUrl: './footer.css'
 })
 export class Footer {
   author = input('unknown');
+  now:Date = new Date(); 
+  constructor(){
+    setInterval(() => {
+      this.now = new Date()
+    }, 3600000)
+  }
 }
