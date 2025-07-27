98cff2d03360f65deaa50dfb8ed64665980769c2
piturria <patricia.iturriaganez@gmail.com>
2025-07-27 20:35:44 +0200

    minor: fixing login form

--- src/app/components/login-form/login-form.ts
+++ src/app/components/login-form/login-form.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { User } from '../../model/user.type';
 
 @Component({
   selector: 'app-login-form',
-  imports: [FormsModule, NgModel],
+  imports: [FormsModule],
   templateUrl: './login-form.html',
   styleUrl: './login-form.css'
 })
 export class LoginForm {
   user : User = { username:'', password:''};
   login() {
-    console.log(`login (${this.user.username}, ${this.user.password})... `)
+    console.log(`login (${this.user.username}, ${this.user.password})... `);
+    debugger;
+    const formValue = this.user;
   }  
 
 }
